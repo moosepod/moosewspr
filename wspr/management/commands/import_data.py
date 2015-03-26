4627207997f2ba37d65ca432f054c4389021dd9f
Matt Christensen <mchristensen@moosepod.com>
2015-03-25 23:49:15 -0400

    Added new summarize command

--- wspr/management/commands/import_data.py
+++ wspr/management/commands/import_data.py
@@ -6,64 +6,69 @@ from django.core.management.base import BaseCommand, CommandError
 from wspr.models import Record
 
 class Command(BaseCommand):
-	args = '<path to csv file> [optional filter]'
-	help = 'Import the WSPR format data at the given path'
-	ROW_LENGTH=15
-	LOG_EVERY=1000
+    args = '<path to csv file> [optional filter]'
+    help = 'Import the WSPR format data at the given path'
+    ROW_LENGTH=15
+    LOG_EVERY=1000
 
-	def _import_row(self,row,filter):
-		(spot_id, timestamp, reporter, reporter_grid, snr,freq,call_sign,grid,power,drift,
-			distance,azimuth,band,version,code) = row
-		if filter:
-			if filter != reporter.lower() and filter != call_sign.lower():
-				return
-		try:
-			record,created = Record.objects.get_or_create(spot_id=int(spot_id),
-				defaults={'timestamp': datetime.datetime.utcfromtimestamp(int(timestamp)).replace(tzinfo=pytz.utc),
-			'reporter': reporter,
-			'reporter_grid': reporter_grid,
-			'snr': int(snr),
-			'frequency': freq,
-			'call_sign': call_sign,
-			'grid': grid,
-			'power': int(power),
-			'drift': int(drift),
-			'distance': int(distance),
-			'azimuth': int(azimuth),
-			'band': int(band),
-			'version': version,
-			'code': int(code)})
-			if created:
-				print 'Added %s, %s->%s' % (spot_id, call_sign,reporter)
-		except ValueError,e:
-			print '   error loading row. %s' % e		
+    def _import_row(self,row,filter):
+        (spot_id, timestamp, reporter, reporter_grid, snr,freq,call_sign,grid,power,drift,
+            distance,azimuth,band,version,code) = row
+        spotdate = datetime.datetime.utcfromtimestamp(int(timestamp)).replace(tzinfo=pytz.utc)
+        if spotdate.day < 23:
+            # Kludge -- hardcoded to the date of the month that I have data past!
+            return
+        if filter:
+            filter = filter.lower()
+            if filter != reporter.lower():
+                return
+        try:
+            record,created = Record.objects.get_or_create(spot_id=int(spot_id),
+                defaults={'timestamp': spotdate,
+            'reporter': reporter,
+            'reporter_grid': reporter_grid,
+            'snr': int(snr),
+            'frequency': freq,
+            'call_sign': call_sign,
+            'grid': grid,
+            'power': int(power),
+            'drift': int(drift),
+            'distance': int(distance),
+            'azimuth': int(azimuth),
+            'band': int(band),
+            'version': version,
+            'code': int(code)})
+            if created:
+                print 'Added %s, %s->%s' % (spot_id, call_sign,reporter)
+        except ValueError,e:
+            print '   error loading row. %s' % e        
 
-	def import_from_file(self,line_reader,filter,max_row=0):
-		count=0
-		for row in line_reader:
-			count+=1
-			if max_row and count > max_row:
-				return
-			if count % Command.LOG_EVERY == 0:
-				print 'Processed %d' % count
-			if len(row) != Command.ROW_LENGTH:
-				print 'Row %d is wrong length. Expected %d, found %d' % (count,Command.ROW_LENGTH,len(row))
-				return
-			self._import_row(row,filter)
+    def import_from_file(self,line_reader,filter,max_row=0):
+        count=0
+        for row in line_reader:
+            count+=1
+            if max_row and count > max_row:
+                return
+            if count % Command.LOG_EVERY == 0:
+                print 'Processed %d' % count
+            if len(row) != Command.ROW_LENGTH:
+                print 'Row %d is wrong length. Expected %d, found %d' % (count,Command.ROW_LENGTH,len(row))
+                return
+            self._import_row(row,filter)
 
-	def handle(self,*args,**kwargs):
-		if len(args) < 1:
-			return Command.help
-		path = args[0]
-		if len(args)> 1:
-			filter = args[1]
-		else:
-			filter = None
-		print 'Processing data at %s' % path
-		if filter: 
-			print 'with callsign filter %s' % filter
-		print '---------------------'
-		with open(path,'rU') as f:
-			self.import_from_file(csv.reader(f),filter,max_row=0)
+    def handle(self,*args,**kwargs):
+        if len(args) < 1:
+            return Command.help
+        path = args[0]
+        if len(args)> 1:
+            filter = args[1]
+        else:
+            filter = None
+        print 'Processing data at %s' % path
+        if filter: 
+            print 'with callsign filter %s' % filter
+        print '---------------------'
+        with open(path,'rU') as f:
+            self.import_from_file(csv.reader(f),filter,max_row=0)
 
-			
+            
--- wspr/models.py
+++ wspr/models.py
@@ -2,11 +2,13 @@ from django.db import models
 from django.db import connection
 
 class RecordManager(models.Manager):
-   def summarize_by_hour(self):
+    def summarize_by_hour(self,reporter,band):
       cursor = connection.cursor()
       cursor.execute("""SELECT year,month,day,hour,count(*)
                         FROM wspr_record
-                        GROUP BY year,month,day,hour""")
+                        WHERE reporter=%s
+                        AND band=%s
+                        GROUP BY year,month,day,hour""",[reporter,band])
       results = []
       row = cursor.fetchone()
       while row:
@@ -14,8 +16,19 @@ class RecordManager(models.Manager):
          row = cursor.fetchone()
       
       return results
+    
+    def by_hour_for_day_as_csv(self,year,month,day,reporter,band):
+        data = self.summarize_by_hour(reporter,band)
+        results = [['%d/%d/%d' % (year,month,day), hour,0] for hour in range(0,24)]
+        for row in data:
+            y,m,d,hour,count = row
+            if year == y and month == m and day == d:
+                results[hour][2] = count
+        for row in results:
+            print '%s,%d,%d' % (row[0],row[1],row[2])
 
-class Record(models.Model):	
+
+class Record(models.Model): 
    spot_id = models.IntegerField(unique=True)
    timestamp = models.DateTimeField()
    reporter = models.CharField(max_length=100)
